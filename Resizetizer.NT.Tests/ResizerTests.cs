6e18ca2382379903001dc86dced66b50721a2c11
Mihails Popovs <mihails.popovs@encontrol.ch>
2020-11-06 09:36:31 +0200

    Added unit test for GetFileDestination method of Resizer

---
+++ Resizetizer.NT.Tests/ResizerTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+
+namespace Resizetizer.NT.Tests
+{
+	public class ResizerTests
+	{
+		private readonly DpiPath _dpiPath =  new DpiPath("", 1);
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		[InlineData("blue_")]
+		public void PrependImageNamePrefix(string prefix)
+		{
+			var info = new SharedImageInfo
+			{
+				Filename = "image.svg",
+				IconNamePrefix = prefix
+			};
+			
+			var fixture = CreateClass(info);
+			var result = fixture.GetFileDestination(_dpiPath);
+			
+			Assert.EndsWith($"{prefix}image.svg", result);
+		}
+		
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		[InlineData("_blue")]
+		public void AppendImageNamePrefix(string postfix)
+		{
+			var info = new SharedImageInfo
+			{
+				Filename = "image.svg",
+				IconNamePostfix = postfix
+			};
+			
+			var fixture = CreateClass(info);
+			var result = fixture.GetFileDestination(_dpiPath);
+			
+			Assert.EndsWith($"image{postfix}.svg", result);
+		}
+
+		private static Resizer CreateClass(SharedImageInfo sharedImageInfo)
+		{
+			return new Resizer(sharedImageInfo, "ResizerTests", new TestLogger());
+		}
+	}
+}
